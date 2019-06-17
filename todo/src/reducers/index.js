a4c4cfdc3c3fd237e22a27d464ceb256e2e93c07
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-17 16:22:46 -0400

    added redux and started outlining structure for the app

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,32 @@
+const initialState = [
+    {
+        value: 'Walk the Dog',
+        completed: false
+    },
+    {
+        value: 'Do the Dishes',
+        completed: false
+    },
+    {
+        value: 'Wash the Car',
+        completed: false
+    }
+]
+
+export const reducer = (state = initialState, action) => {
+    console.log(action);
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    value: action.payload,
+                    completed: false
+                }
+            ]
+        case 'TOGGLE_TODO':
+            return state.map(todo => (todo.value === action.value) ? { ...todo, completed: !todo.completed } : todo)
+        default:
+            return state
+    }
+}
