fe143fd1a634645addb65fddc43939e062b33498
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-18 16:29:04 -0400

    added a modal to confirm deleting an item not marked as complete

--- todo/src/components/ToDoItem.js
+++ todo/src/components/ToDoItem.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { ListGroupItem, Button } from 'reactstrap';
+import { ListGroupItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const ToDoItem = props => {
-    console.log(props.toggleToDo)
-    return(
-        <ListGroupItem>
-            <span onClick={() => props.toggleToDo(props.toDoProp)} style={{textDecoration: props.toDoProp.completed ? 'line-through' : 'none'}}>{props.toDoProp.value}</span>
-            <Button onClick={() => props.deleteToDo(props.toDoProp)} size='sm' color='danger' className='float-right'>Delete</Button>
-        </ListGroupItem>
-    )
+class ToDoItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        }
+    }
+
+    toggle = () => {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }))
+    }
+
+    render() {
+        return(
+            <ListGroupItem>
+                <span onClick={() => this.props.toggleToDo(this.props.toDoProp)} style={{textDecoration: this.props.toDoProp.completed ? 'line-through' : 'none'}}>{this.props.toDoProp.value}</span>
+                <Button onClick={
+                    (this.props.toDoProp.completed) ? () => this.props.deleteToDo(this.props.toDoProp) : this.toggle
+                } 
+                
+                size='sm' color='danger' className='float-right'>Delete</Button>
+
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>Warning!</ModalHeader>
+                    <ModalBody>
+                        The item you are about to delete has not been marked as completed. Are you sure you want to delete it?
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color='danger' onClick={() => this.props.deleteToDo(this.props.toDoProp)} className='mr-2'>Delete</Button>
+                        <Button color='secondary' onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </ListGroupItem>
+        )
+    }
 }
 
 export default ToDoItem;
